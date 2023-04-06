5559074c350dc386761683ecb15c175d8fae8bc6
paulorb <paulorbhell@gmail.com>
2023-04-06 01:01:35 -0300

    Added basic packet validation

--- src/main/kotlin/ModbusPacket.kt
+++ src/main/kotlin/ModbusPacket.kt
@@ -1,3 +1,7 @@
+import com.sun.media.sound.InvalidFormatException
+import com.sun.org.slf4j.internal.LoggerFactory
+import java.util.logging.Logger
+
 class ModbusPacket(
     var transactionIdentifier: Short,
     var protocolIdentifier: Short,
@@ -6,4 +10,15 @@ class ModbusPacket(
     var functionCode: Byte,
     var byteVector: ByteArray
     ) {
+
+    companion object {
+        val logger = LoggerFactory.getLogger(ModbusPacket::class.java)
+    }
+
+    init {
+        logger.debug("Packet received: trans ind: $transactionIdentifier prot ind: $protocolIdentifier unitID $unitID functionCode $functionCode")
+        if(lenght < byteVector.size + 2){
+            throw InvalidFormatException("Invalid size")
+        }
+    }
 }
