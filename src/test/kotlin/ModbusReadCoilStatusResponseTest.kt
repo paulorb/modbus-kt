655bcf4755c0760e12def19104c82a5a089511e0
paulorb <paulorbhell@gmail.com>
2023-04-12 11:26:21 -0300

    Added more unit test for ModbusReadCoilStatusResponse

--- src/test/kotlin/ModbusReadCoilStatusResponseTest.kt
+++ src/test/kotlin/ModbusReadCoilStatusResponseTest.kt
@@ -78,6 +78,32 @@ internal class ModbusReadCoilStatusResponseTest {
         assertEquals(0, buffer[6])  // Unit identifier
         assertEquals(1, buffer[7])  // Function code
         assertEquals(1, buffer[8])  // Number of bytes to follow
-        assertEquals(0xaa, buffer[9].toUByte().toInt())  // Number of bytes to follow
+        assertEquals(0x55, buffer[9].toUByte().toInt())  // Number of bytes to follow
+    }
+
+    @Test
+    fun read8CoilsAnotherMixTrueAndFalse() {
+        var response = ModbusReadCoilStatusResponse()
+        response.setCoil(0, true)
+        response.setCoil(1, true)
+        response.setCoil(2, true)
+        response.setCoil(3, false)
+        response.setCoil(4, false)
+        response.setCoil(5, false)
+        response.setCoil(6, false)
+        response.setCoil(7, false)
+        response.transactionIdentifier = 1
+        var buffer = response.toProto()
+        assertEquals(10, buffer.size)
+        assertEquals(0, buffer[0])  // Transaction ID
+        assertEquals(1, buffer[1])  // Transaction ID
+        assertEquals(0, buffer[2])  // Protocol Identifier
+        assertEquals(0, buffer[3])  // Protocol Identifier
+        assertEquals(0, buffer[4])  // Message length
+        assertEquals(4, buffer[5])  // Message length
+        assertEquals(0, buffer[6])  // Unit identifier
+        assertEquals(1, buffer[7])  // Function code
+        assertEquals(1, buffer[8])  // Number of bytes to follow
+        assertEquals(0x7, buffer[9].toUByte().toInt())  // Number of bytes to follow
     }
 }
