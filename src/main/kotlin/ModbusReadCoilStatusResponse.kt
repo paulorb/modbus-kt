48d762758877e00f24c67d38ff343cadd7424f25
paulorb <paulorbhell@gmail.com>
2023-05-03 15:59:16 -0300

    Fixed issue with 0x registers

---
+++ src/main/kotlin/ModbusReadCoilStatusResponse.kt
@@ -0,0 +1,77 @@
+import ModbusPacket.FunctionCode.Companion.DEFAULT_PROTOCOL_IDENTIFIER
+import kotlin.experimental.or
+import kotlin.math.max
+
+class ModbusReadCoilStatusResponse: ModbusPacket {
+    constructor(transactionIdentifier: Short,
+                protocolIdentifier: Short,
+                lenght: Short,
+                unitID: Byte,
+                functionCode: Byte,
+                byteVector: ByteArray
+        ): super(transactionIdentifier, protocolIdentifier, lenght, unitID, functionCode, byteVector)
+    constructor() : super(0,0,0,0,0, ByteArray(0))
+
+    companion object {
+        const val LEN_BYTES_METADATA_SIZE = 1
+    }
+
+    private var listIndexToCoilValue = mutableListOf<Pair<Int,Boolean>>()
+
+
+    private fun getBit(value: Byte, position: Int): Boolean {
+        return (value.toInt() shr position and 1) == 1
+    }
+
+    private fun setBit(value: Byte, position: Int): Byte {
+        return value or ((1 shl position).toByte())
+    }
+
+    fun setCoil(bitIndex: Int, value: Boolean){
+        listIndexToCoilValue.add(Pair<Int, Boolean>(bitIndex, value))
+    }
+
+    //@Throws(ArrayIndexOutOfBoundsException)
+    fun getCoil(index: Int): Boolean {
+        return getBit(byteVector[index / 8], index % 8)
+    }
+
+    fun extraByte(value: Int) : Int {
+        return if(value % 8  == 0){
+            0
+        }else{
+            1
+        }
+    }
+
+    override fun encode() {
+        functionCode = ModbusPacket.FunctionCode.READ_COIL_STATUS.value.toByte()
+        protocolIdentifier = DEFAULT_PROTOCOL_IDENTIFIER.toShort()
+
+        var maxIndex = 0
+        var minIndex = Short.MAX_VALUE.toInt()
+        for(element in listIndexToCoilValue){
+            if(element.first > maxIndex){
+                maxIndex =  element.first
+            }
+            if(element.first < minIndex){
+                minIndex = element.first
+            }
+        }
+        println("min $minIndex max $maxIndex")
+        val numberOfBytes = ((maxIndex.toInt() - minIndex.toInt() + 1)/8) + extraByte((maxIndex.toInt() - minIndex.toInt() + 1))
+        val length = max(numberOfBytes, 1).toByte()
+        println("modbus length $length")
+        byteVector = ByteArray(LEN_BYTES_METADATA_SIZE + length )
+        byteVector[0] = length
+        for(element in listIndexToCoilValue){
+            if(element.second) {
+                println("index ${(element.first / 8) + 1 - minIndex} bit index ${element.first % 8}")
+                byteVector[((element.first - minIndex)/8)+ 1] =
+                    setBit(byteVector[((element.first - minIndex)/8)+ 1], element.first % 8)
+            }
+        }
+    }
+
+    //TODO when implement the client remember to extend this class to also support decoding
+}
--- src/test/kotlin/ModbusReadCoilStatusResponseTest.kt
+++ src/test/kotlin/ModbusReadCoilStatusResponseTest.kt
@@ -28,6 +28,58 @@ internal class ModbusReadCoilStatusResponseTest {
         assertEquals(0xFF, buffer[9].toUByte().toInt())  // Number of bytes to follow
     }
 
+    @Test
+    fun read8CoilsStartingFromReg100AllTrue() {
+        var response = ModbusReadCoilStatusResponse()
+        response.setCoil(104, true)
+        response.setCoil(105, true)
+        response.setCoil(106, true)
+        response.setCoil(107, true)
+        response.setCoil(108, true)
+        response.setCoil(109, true)
+        response.setCoil(110, true)
+        response.setCoil(111, true)
+        response.transactionIdentifier = 1
+        var buffer = response.toProto()
+        assertEquals(10, buffer.size)
+        assertEquals(0, buffer[0])  // Transaction ID
+        assertEquals(1, buffer[1])  // Transaction ID
+        assertEquals(0, buffer[2])  // Protocol Identifier
+        assertEquals(0, buffer[3])  // Protocol Identifier
+        assertEquals(0, buffer[4])  // Message length
+        assertEquals(4, buffer[5])  // Message length
+        assertEquals(0, buffer[6])  // Unit identifier
+        assertEquals(1, buffer[7])  // Function code
+        assertEquals(1, buffer[8])  // Number of bytes to follow
+        assertEquals(0xFF, buffer[9].toUByte().toInt())  // Number of bytes to follow
+    }
+
+    @Test
+    fun read8CoilsAnotherStartingFromReg100AllTrue() {
+        var response = ModbusReadCoilStatusResponse()
+        response.setCoil(100, true)
+        response.setCoil(101, true)
+        response.setCoil(102, true)
+        response.setCoil(103, true)
+        response.setCoil(104, true)
+        response.setCoil(105, true)
+        response.setCoil(106, true)
+        response.setCoil(107, true)
+        response.transactionIdentifier = 1
+        var buffer = response.toProto()
+        assertEquals(10, buffer.size)
+        assertEquals(0, buffer[0])  // Transaction ID
+        assertEquals(1, buffer[1])  // Transaction ID
+        assertEquals(0, buffer[2])  // Protocol Identifier
+        assertEquals(0, buffer[3])  // Protocol Identifier
+        assertEquals(0, buffer[4])  // Message length
+        assertEquals(4, buffer[5])  // Message length
+        assertEquals(0, buffer[6])  // Unit identifier
+        assertEquals(1, buffer[7])  // Function code
+        assertEquals(1, buffer[8])  // Number of bytes to follow
+        assertEquals(0xFF, buffer[9].toUByte().toInt())  // Number of bytes to follow
+    }
+
 
     @Test
     fun read8CoilsAllFalse() {
@@ -106,4 +158,115 @@ internal class ModbusReadCoilStatusResponseTest {
         assertEquals(1, buffer[8])  // Number of bytes to follow
         assertEquals(0x7, buffer[9].toUByte().toInt())  // Number of bytes to follow
     }
+
+
+    @Test
+    fun read16CoilsAllTrue() {
+        var response = ModbusReadCoilStatusResponse()
+        response.setCoil(0, true)
+        response.setCoil(1, true)
+        response.setCoil(2, true)
+        response.setCoil(3, true)
+        response.setCoil(4, true)
+        response.setCoil(5, true)
+        response.setCoil(6, true)
+        response.setCoil(7, true)
+        response.setCoil(8, true)
+        response.setCoil(9, true)
+        response.setCoil(10, true)
+        response.setCoil(11, true)
+        response.setCoil(12, true)
+        response.setCoil(13, true)
+        response.setCoil(14, true)
+        response.setCoil(15, true)
+        response.transactionIdentifier = 1
+        var buffer = response.toProto()
+        assertEquals(11, buffer.size)
+        assertEquals(0, buffer[0])  // Transaction ID
+        assertEquals(1, buffer[1])  // Transaction ID
+        assertEquals(0, buffer[2])  // Protocol Identifier
+        assertEquals(0, buffer[3])  // Protocol Identifier
+        assertEquals(0, buffer[4])  // Message length
+        assertEquals(5, buffer[5])  // Message length
+        assertEquals(0, buffer[6])  // Unit identifier
+        assertEquals(1, buffer[7])  // Function code
+        assertEquals(2, buffer[8])  // Number of bytes to follow
+        assertEquals(0xFF, buffer[9].toUByte().toInt())  // Number of bytes to follow
+        assertEquals(0xFF, buffer[10].toUByte().toInt())  // Number of bytes to follow
+    }
+
+    @Test
+    fun read16CoilsMixTrueAndFalse() {
+        var response = ModbusReadCoilStatusResponse()
+        response.setCoil(0, true)
+        response.setCoil(1, false)
+        response.setCoil(2, true)
+        response.setCoil(3, false)
+        response.setCoil(4, true)
+        response.setCoil(5, false)
+        response.setCoil(6, true)
+        response.setCoil(7, false)
+        response.setCoil(8, true)
+        response.setCoil(9, false)
+        response.setCoil(10, true)
+        response.setCoil(11, false)
+        response.setCoil(12, true)
+        response.setCoil(13, false)
+        response.setCoil(14, true)
+        response.setCoil(15, false)
+        response.transactionIdentifier = 1
+        var buffer = response.toProto()
+        assertEquals(11, buffer.size)
+        assertEquals(0, buffer[0])  // Transaction ID
+        assertEquals(1, buffer[1])  // Transaction ID
+        assertEquals(0, buffer[2])  // Protocol Identifier
+        assertEquals(0, buffer[3])  // Protocol Identifier
+        assertEquals(0, buffer[4])  // Message length
+        assertEquals(5, buffer[5])  // Message length
+        assertEquals(0, buffer[6])  // Unit identifier
+        assertEquals(1, buffer[7])  // Function code
+        assertEquals(2, buffer[8])  // Number of bytes to follow
+        assertEquals(0x55, buffer[9].toUByte().toInt())  // Number of bytes to follow
+        assertEquals(0x55, buffer[10].toUByte().toInt())  // Number of bytes to follow
+    }
+
+    @Test
+    fun read80CoilsMixTrueAndFalse() {
+        var response = ModbusReadCoilStatusResponse()
+        var bitIndex = 0
+        for(i in 0..9) {
+            response.setCoil(bitIndex++, true)
+            response.setCoil(bitIndex++, false)
+            response.setCoil(bitIndex++, true)
+            response.setCoil(bitIndex++, false)
+            response.setCoil(bitIndex++, true)
+            response.setCoil(bitIndex++, false)
+            response.setCoil(bitIndex++, true)
+            response.setCoil(bitIndex++, false)
+        }
+        response.transactionIdentifier = 1
+        var buffer = response.toProto()
+        assertEquals(19, buffer.size)
+        assertEquals(0, buffer[0])  // Transaction ID
+        assertEquals(1, buffer[1])  // Transaction ID
+        assertEquals(0, buffer[2])  // Protocol Identifier
+        assertEquals(0, buffer[3])  // Protocol Identifier
+        assertEquals(0, buffer[4])  // Message length
+        assertEquals(13, buffer[5])  // Message length
+        assertEquals(0, buffer[6])  // Unit identifier
+        assertEquals(1, buffer[7])  // Function code
+        assertEquals(10, buffer[8])  // Number of bytes to follow
+        assertEquals(0x55, buffer[9].toUByte().toInt())  // Number of bytes to follow
+        assertEquals(0x55, buffer[10].toUByte().toInt())  // Number of bytes to follow
+        assertEquals(0x55, buffer[11].toUByte().toInt())  // Number of bytes to follow
+        assertEquals(0x55, buffer[12].toUByte().toInt())  // Number of bytes to follow
+        assertEquals(0x55, buffer[13].toUByte().toInt())  // Number of bytes to follow
+        assertEquals(0x55, buffer[14].toUByte().toInt())  // Number of bytes to follow
+        assertEquals(0x55, buffer[15].toUByte().toInt())  // Number of bytes to follow
+        assertEquals(0x55, buffer[16].toUByte().toInt())  // Number of bytes to follow
+        assertEquals(0x55, buffer[17].toUByte().toInt())  // Number of bytes to follow
+        assertEquals(0x55, buffer[18].toUByte().toInt())  // Number of bytes to follow
+
+    }
+
 }
