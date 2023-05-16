30d9748ccaa6a11fb418503a46adf4260e5b6617
paulorb <paulorbhell@gmail.com>
2023-05-16 13:43:38 -0300

    Added gradle files

---
+++ build.gradle.kts
@@ -0,0 +1,44 @@
+plugins {
+    kotlin("jvm") version "1.8.0"
+    application
+}
+
+group = "org.example"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(kotlin("test"))
+    implementation("io.netty:netty-all:4.1.24.Final")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+
+kotlin {
+    jvmToolchain(8)
+}
+
+application {
+    mainClass.set("MainKt")
+}
+
+tasks.withType<Jar> {
+    manifest {
+        attributes["Main-Class"] = "MainKt"
+    }
+    // To avoid the duplicate handling strategy error
+    duplicatesStrategy = DuplicatesStrategy.EXCLUDE
+
+    // To add all of the dependencies
+    from(sourceSets.main.get().output)
+
+    dependsOn(configurations.runtimeClasspath)
+    from({
+        configurations.runtimeClasspath.get().filter { it.name.endsWith("jar") }.map { zipTree(it) }
+    })
+}
---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,34 @@
+import io.netty.bootstrap.ServerBootstrap
+import io.netty.channel.ChannelFuture
+import io.netty.channel.ChannelOption
+import io.netty.channel.EventLoopGroup
+import io.netty.channel.nio.NioEventLoopGroup
+import io.netty.channel.socket.nio.NioServerSocketChannel
+import io.netty.handler.logging.LogLevel
+import io.netty.handler.logging.LoggingHandler
+
+fun main(args: Array<String>) {
+    // Configure the server.
+    val bossGroup: EventLoopGroup = NioEventLoopGroup(1)
+    val workerGroup: EventLoopGroup = NioEventLoopGroup()
+    val serverHandler = ModbusServerHandler()
+    val serverChannelInitializer = ServerChannelInitializer(serverHandler)
+    try {
+        val b = ServerBootstrap()
+        b.group(bossGroup, workerGroup)
+            .channel(NioServerSocketChannel::class.java)
+            .option(ChannelOption.SO_BACKLOG, 100)
+            .handler(LoggingHandler(LogLevel.INFO))
+            .childHandler(serverChannelInitializer)
+
+        // Start the server.
+        val f: ChannelFuture = b.bind(5002).sync()
+
+        // Wait until the server socket is closed.
+        f.channel().closeFuture().sync()
+    } finally {
+        // Shut down all event loops to terminate all threads.
+        bossGroup.shutdownGracefully()
+        workerGroup.shutdownGracefully()
+    }
+}
