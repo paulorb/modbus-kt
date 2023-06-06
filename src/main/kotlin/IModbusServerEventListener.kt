afdb03c7266c070113e452ac53884e2bfdcd79bb
paulorb <paulorbhell@gmail.com>
2023-06-06 07:53:42 +0400

    Add interface to handle events of the ModbusServerHandler

---
+++ src/main/kotlin/IModbusServerEventListener.kt
@@ -0,0 +1,11 @@
+interface IModbusServerEventListener {
+    fun readCoilStatus(startAddress: Int ,numberOfRegisters: Int) : List<Boolean>
+    fun readInputStatus(startAddress: Int ,numberOfRegisters: Int) : List<Boolean>
+    fun readHoldingRegister(startAddress: Int ,numberOfRegisters: Int) : List<Short>
+    fun readInputRegister(startAddress: Int ,numberOfRegisters: Int) : List<Short>
+    fun forceSingleCoil(address: Int, value: Boolean)
+    fun presetSingleRegister(address: Int, value: Boolean)
+    fun forceMultipleCoils(addressValueList: MutableList<Pair<Int, Boolean>>)
+    fun presetMultipleRegisters(addressValueList: MutableList<Pair<Int, Short>>)
+
+}
